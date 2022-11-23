4629e4e0192f1ac19561253e8b59bf0efce9fc82
jinjuwon <drasgon@naver.com>
2022-11-23 10:56:06 +0900

    'EatUsAPI setting first'

---
+++ src/main/java/com/example/eatusapi/EatUsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatUsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatUsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eatusapi/EatUsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatUsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
