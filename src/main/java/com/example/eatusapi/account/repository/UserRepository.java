7852b21e1e6051d3815dc5465e3c9e588b66cdc1
jinjuwon <drasgon@naver.com>
2022-11-28 10:25:01 +0900

    'EatUsAPI setting first'

--- src/main/java/com/example/eatusapi/account/repository/UserRepository.java
+++ src/main/java/com/example/eatusapi/account/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.eatusapi.account.repository;
 import com.example.eatusapi.account.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
@@ -11,4 +12,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     @Override
     User getReferenceById(Long id);
+    List<User> getUsersByUserName(String userName);
 }
